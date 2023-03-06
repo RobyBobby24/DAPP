f562c1e526d19f2c26b4af4760bf81e1ae0ed735
Matteopast01 <matteopast@gmail.com>
2023-03-06 23:01:45 +0100

    modifica di alcuni aspetti dell'abilità

--- src/main/java/model/StandardRoomFactory.java
+++ src/main/java/model/StandardRoomFactory.java
@@ -13,10 +13,6 @@ public class StandardRoomFactory implements RoomFactory {
 	 */
 	public BattleRoom createBattleRoom() {
 		BattleRoom room = new BattleRoom();
-		TreeMap<String,String> attribute_value = new TreeMap<>();
-		attribute_value.put("name","Browser");
-		List<Monster> monsters = PersistenceInterface.getInstance().search(attribute_value,Monster.class);
-		room.setMonsters(monsters);
 		return room;
 	}
 
